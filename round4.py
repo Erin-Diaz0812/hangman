fb33ad8e77df372f08ff60decbc679a8ec0c7eff
Erin-Diaz0812 <a02348502@usu.edu>
2026-03-01 01:47:56 +0000

    cleaning up round4.py of all commented out code sections

--- round4.py
+++ round4.py
@@ -16,38 +16,7 @@ def log_game_results(chosen_word, category, guesses_used, guess_count, won, gues
 
 df = pd.read_csv('hangman_word_list.csv')
 
-#print(df) commenting out dataframe to make terminal more manageable
-
-#just me seeing if the base loop works. This is randomly selecting from the ENTIRE list
-# chosen_word = df['word'].sample(1).values[0] #df['word'] = grab word column; sample(1) = pick a random row; value[0] = extraction to plain string
-#print(chosen_word)
-
 print("Welcome to my rendition of hangman!")
-
-'''
-initial filtering idea:
-#allow for game customization: categories, word length, randomization, guess counter
-selection = input("Would you like to choose the category, word length, or amount of guesses you'll have during this session? Please enter Yes or No: ")
-
-if selection == 'Yes':
-    selection2 = input("Which of the options would you like to pick from? \nCategories \n Word length \n Guess count")
-    
-    if selection2 == "Categories":
-        categories = ['animal', 'dessert', 'clothing', 'vehicles', 'foreign word']
-        print("Categories: ", categories)
-        chosen_category = input("Please select a category: ")
-
-    if selection2 == "Word length":
-        minimum_length = int(input("What is the minimum word length you want (3 to 11)? "))
-        maximum_length = int(input("What isi the maximum word length you want (3 to 11)? "))
-        
-    if selection2 == "Guess count":
-        guess_count = int(input("How many guesses would you like (recommend between 6 to 10)? "))
-
-else:
-    chosen_word = df['word'].sample(1).values[0] #df['word'] = grab word column; sample(1) = pick a random row; value[0] = extraction to plain string
-    print(chosen_word)
-'''
 
 chosen_category = None
 minimum_length = 3
@@ -55,8 +24,7 @@ maximum_length = 11
 guess_count = 10
 attempts_left = guess_count
 
-#print(df.columns.tolist()) GenAI suggestion to see what python/csv is reading the column headers
-
+#doing game set up
 selection = input("Would you like to customize this game? (Yes or No) ")
 
 if selection == "Yes":
@@ -84,17 +52,14 @@ if chosen_category:
     ]
 
 chosen_word = filtered_df['word'].sample(1).values[0]
-print(chosen_category) #figure out a way to print out category if they reject customizing the game; this will have to be a DF thing
+print(chosen_category) 
 print(guess_count)
-#print(chosen_word) #will need to comment out
 
 #actually turning this playable
 display = []
 for letter in chosen_word:
     display += '_'
 print(display)
-
-#guess_count = 10 #swap to optional input later
 
 banned_letters = []
 guessed_letters = []
@@ -126,7 +91,6 @@ while guess_count > 0 and '_' in display:
     if good_guess:
         print("you guessed: ", guess_letter)
         print(display)
-        #guess_count -=1 Don't want to remove guesses for correct ones
         print("There are now: ", guess_count, "guesses remaining")
         print("Incorrect guessed letters are: ", *banned_letters)
 
@@ -151,6 +115,8 @@ else:
 
 
 '''
+trying and failing to do a game loop. It broke too many things and took me about 2 hours before deciding that it won't be worth it.
+
         do_again = input("Would you like to play again? (Yes or No) ")
         if do_again != "Yes":
             break
@@ -170,9 +136,3 @@ if play_again == "Yes":
         display += '_'
         print(display)
 '''
-
-
-#inaccurate way to do this now
-#chosen_word = random.hangman_word_list.csv
-#print(chosen_word)
-
